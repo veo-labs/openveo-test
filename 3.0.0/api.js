79d22fa7e7b922dfcfd83e28ef57055b503ac0b9
Patrick Champclos <patrick.champclos@veo-labs.com>
2016-05-30 11:28:08 +0200

    Auto-generated documentation for version 3.0.0

---
+++ 3.0.0/api.js
@@ -0,0 +1,47 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "BackEndPage",
+        "CheckboxField",
+        "CheckboxesField",
+        "DateField",
+        "Field",
+        "Helper",
+        "Page",
+        "SelectField",
+        "TableAssert",
+        "TablePage",
+        "TagsField",
+        "TextField",
+        "TextareaField",
+        "TimeField",
+        "browser",
+        "generator",
+        "i18n",
+        "util"
+    ],
+    "modules": [
+        "e2e",
+        "unit",
+        "util"
+    ],
+    "allModules": [
+        {
+            "displayName": "e2e",
+            "name": "e2e",
+            "description": "Exposes a list of modules to help writing end to end tests on OpenVeo using protractor."
+        },
+        {
+            "displayName": "unit",
+            "name": "unit",
+            "description": "Exposes a list of modules to help writing server unit tests on OpenVeo using mocha."
+        },
+        {
+            "displayName": "util",
+            "name": "util",
+            "description": "Exposes a list of functions to help writing tests."
+        }
+    ],
+    "elements": []
+} };
+});
